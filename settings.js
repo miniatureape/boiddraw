b7a0bae99d33da250e2a79d0c5b8f39243743633
Justin Donato <justin.donato@gmail.com>
2018-04-21 22:25:13 -0400

    change pallete

--- settings.js
+++ settings.js
@@ -1,13 +1,18 @@
 function Settings(root, settings) {
 
+    // Paste this in the console of a color lovers page to get the rgba values
+    // Array.prototype.slice.call(document.querySelectorAll("div.meta > div.col-80.big-number-label > h4")).map(function(i) { return "\"rgba(" + i.innerHTML + ",1)\"" }).join(",\n")
+
     let defaults = {
         numboids: 5,
         maxSpeed: 6,
         maxForce: 4,
         pallete: [
-            "rgba(232,221,203,1)",
-            "rgba(205,179,128,1)",
-            "rgba(3,101,100,1)",
+            "rgba(250,208,137,1)",
+            "rgba(255,156,91,1)",
+            "rgba(245,99,74,1)",
+            "rgba(237,48,60,1)",
+            "rgba(59,129,131,1)"
         ],
     };
 
@@ -15,4 +20,5 @@ function Settings(root, settings) {
         settings: Object.assign(defaults, settings),
     };
 
+    // "div.meta > div.col-80.big-number-label > h4"
 }
